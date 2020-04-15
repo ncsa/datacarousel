0ee151f415c3b843f95c3f6073ba16c277963fb1
Ben Galewsky <ben@peartreestudio.net>
2020-04-15 09:47:09 -0500

    Initial Story 1

---
+++ README.md
@@ -0,0 +1,63 @@
+# Data Carousel
+This implements an AWS service that allows researchers to analyze HDF5 data 
+stored in AWS Glacier archive. It does this by creating a regular process for
+restoring the data to S3 and running researcher-supplied batch jobs in Amazon's
+Elastic Container Serivce against that data.
+
+## AWS Dependent Environment
+The application is provided as a CloudFormation template. This template is 
+produced using the 
+[troposphere cloud library](https://github.com/cloudtools/troposphere).
+
+The chart assumes the existence of a Virtual Private Cloud, a public subnet, 
+and a default VPC security group.
+
+These can be created with the AWS Console. There is a VPC wizard that can be 
+launched from the VPC Dashboard. The simple setup is the 
+_VPC with a single public subnet_ option. Give your VPC a name and accept the
+rest of the defaults. 
+
+Edit the generated public subnet and set _Auto-assign public IPv4 address_ to
+True.
+
+Note the ID of this generated public subnet along with the VPC's default 
+security group.
+
+## Build CloudFormation Template
+We use the Troposhere python DSL to reduce the complexity of creating 
+CloudFormation templates. You can generate new templates with
+```shell script
+pip install -r requirements.txt
+python stack.py > stack.json
+```
+
+## Create the Data Carousel with CloudFormation
+1. Go to the CloudFormation page in the AWS Console and click on _Create Stack_
+2. Select _Template is Ready_
+3. Select _Upload a Template file_
+4. Choose the generated `stack.json` file and click _Next_
+5. Enter a name for your data carousel stack
+6. Paste the ID of the generated public Subnet as _Subnet A_
+7. Select your new VPC and that VPC's default Security Group in the other fields
+8. Click Next and then Next and
+ _acknowledge that AWS CloudFormation might create IAM resources._
+ 9. Click _Create Stack_ and wait.....
+ 
+ ## Run a Radience Batch Job
+ We have a single sample batch job based on an
+ [example from Yizhe Zhan](https://github.com/uiucYizhe/ClimateMarble). To
+ run it, vist the AWS Batch page in the console and on the Dashboard, select
+ _Create Job_.
+ 
+ Name your batch job, select the `ComputeRadience` job definition, the 
+ `datacarousel-job-queue` and accept the rest of the default values. Click on
+ _Submit Job_.
+ 
+ You can visit the EC2 page in the console to see the server start up to run
+ the container. You can monitor the job's progress from the Batch dashboard.
+ 
+ When the job completes, it will store a compressed numpy file in a bucket in 
+ your S3 account.
+ 
+
+
---
+++ stack.py
@@ -0,0 +1,141 @@
+from troposphere import Ref, Template, Parameter, Tags, Output
+from troposphere.batch import ComputeEnvironment, ComputeResources, \
+    JobQueue, ComputeEnvironmentOrder, JobDefinition, \
+    ContainerProperties
+from troposphere.ec2 import SecurityGroup
+from troposphere.iam import Role, InstanceProfile
+
+t = Template()
+
+SubnetA = t.add_parameter(Parameter(
+    'SubnetA',
+    Type='String'
+))
+
+VpcSecurityGroup = t.add_parameter(Parameter(
+    'VpcSecurityGroup',
+    Description="The VPCs default Security Group",
+    ConstraintDescription='Must be a valid Security Group ID.',
+    Type='AWS::EC2::SecurityGroup::Id'
+))
+
+
+Vpc = t.add_parameter(Parameter(
+    'Vpc',
+    ConstraintDescription='Must be a valid VPC ID.',
+    Type='AWS::EC2::VPC::Id'
+))
+
+BatchServiceRole = t.add_resource(Role(
+    'BatchServiceRole',
+    Path='/',
+    Policies=[],
+    ManagedPolicyArns=[
+        'arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole',
+    ],
+    AssumeRolePolicyDocument={'Statement': [{
+        'Action': ['sts:AssumeRole'],
+        'Effect': 'Allow',
+        'Principal': {'Service': ['batch.amazonaws.com']}
+    }]},
+))
+
+BatchInstanceRole = t.add_resource(Role(
+    'BatchInstanceRole',
+    Path='/',
+    Policies=[],
+    ManagedPolicyArns=[
+        'arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role',  # NOQA
+        'arn:aws:iam::aws:policy/AmazonS3FullAccess'
+    ],
+    AssumeRolePolicyDocument={'Statement': [{
+        'Action': ['sts:AssumeRole'],
+        'Effect': 'Allow',
+        'Principal': {'Service': ['ec2.amazonaws.com']}
+    }]},
+))
+
+BatchJobRole = t.add_resource(Role(
+    'BatchJobRole',
+    Path='/',
+    Policies=[],
+    ManagedPolicyArns=[
+        'arn:aws:iam::aws:policy/AmazonS3FullAccess'
+    ],
+    AssumeRolePolicyDocument={'Statement': [{
+        'Action': ['sts:AssumeRole'],
+        'Effect': 'Allow',
+        'Principal': {'Service': ['ecs-tasks.amazonaws.com']}
+    }]},
+))
+
+BatchInstanceProfile = t.add_resource(InstanceProfile(
+    'BatchInstanceProfile',
+    Path='/',
+    Roles=[Ref(BatchInstanceRole)],
+))
+
+BatchSecurityGroup = t.add_resource(SecurityGroup(
+    'BatchSecurityGroup',
+    VpcId=Ref(Vpc),
+    GroupDescription='Enable access to Batch instances',
+    Tags=Tags(Name='batch-sg')
+))
+
+BatchComputeEnvironment = t.add_resource(ComputeEnvironment(
+    'DataCarouselComputeEnvironment',
+    Type='MANAGED',
+    ServiceRole=Ref(BatchServiceRole),
+    ComputeResources=ComputeResources(
+        'DataCarouselComputeResources',
+        Type='EC2',
+        DesiredvCpus=1,
+        MinvCpus=0,
+        MaxvCpus=10,
+        InstanceTypes=['m4.large'],
+        InstanceRole=Ref(BatchInstanceProfile),
+        SecurityGroupIds=[Ref(VpcSecurityGroup)],
+        Subnets=[
+            Ref(SubnetA)
+        ],
+        Tags=dict(
+            Name='batch-compute-environment',
+            Project='NASA Data Carousel'
+        )
+    )
+))
+
+DataCarouselJobQueue = t.add_resource(JobQueue(
+    'DataCarouselJobQueue',
+    ComputeEnvironmentOrder=[
+        ComputeEnvironmentOrder(
+            ComputeEnvironment=Ref(BatchComputeEnvironment),
+            Order=1
+        ),
+    ],
+    Priority=1,
+    State='ENABLED',
+    JobQueueName='datacarousel-job-queue'
+))
+
+command = 'python work_flow.py 2002 12 s3://terrafusiondatasampler/P22/TERRA_BF_L1B_O85079_20151216160910_F000_V001.h5'
+t.add_resource(JobDefinition(
+    "ComputeRadience",
+    Type='Container',
+    ContainerProperties=ContainerProperties(
+        Command=command.split(' '),
+        Image='bengal1/climatemarble:latest',
+        Memory=4096,
+        Vcpus=1,
+        JobRoleArn=Ref(BatchJobRole)
+    )
+))
+
+
+t.add_output([
+    Output('DataCarouselComputeEnvironment', Value=Ref(BatchComputeEnvironment)),
+    Output('BatchSecurityGroup', Value=Ref(BatchSecurityGroup)),
+    Output('DataCarouselJobQueue', Value=Ref(DataCarouselJobQueue))
+])
+
+print(t.to_json())
